5006df2aa05f72037b6aac5f782530e0cafb578c
Давлатов Шахзод <shahzod@davlatov.space>
2022-07-14 13:31:28 +0300

    Обновлена проверка на браузер

--- source/hooks/useUserAgent.ts
+++ source/hooks/useUserAgent.ts
@@ -2,10 +2,10 @@ import { useRef } from 'react';
 
 const useUserAgent = () => {
   const userAgent = useRef(navigator.userAgent).current;
-  const vendor = useRef(navigator.vendor).current;
-  const regexp = /Mobile.*Safari/;
+  const regexpSafari = /^((?!chrome|android|crios|fxios).)*safari/i;
+  const regexpMobileSafari = /Mobile.*Safari/;
 
-  const isIosSafari = regexp.test(userAgent) && vendor === 'Apple Computer, Inc.';
+  const isIosSafari = regexpSafari.test(userAgent) && regexpMobileSafari.test(userAgent);
 
   return { isIosSafari };
 };
